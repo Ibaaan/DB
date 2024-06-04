4d472b180f7d2acc2a367a701938b14bea3b947c
Ivan <90986696+Ibaaan@users.noreply.github.com>
2024-06-04 20:56:58 +0500

    comment

--- Base/ArrayList.h
+++ Base/ArrayList.h
@@ -1,141 +1,181 @@
+// Include necessary headers for the implementation
 #pragma once
 #include <iostream>
 #include <memory>
 #include <initializer_list>
 
+// Define a Node template struct to hold data and id
 template<typename T>
 struct Node {
-	T data;
-	int id;
-	Node(const T data, int id) : data(data), id(id) {}
-	Node(const Node& other) : data(other.data), id(other.id) {}
-	Node() {}
+    T data; // The data stored in the node
+    int id;  // The unique identifier for the node
+
+    // Constructor for initializing the node with data and id
+    Node(const T data, int id) : data(data), id(id) {}
+
+    // Copy constructor for creating a copy of the node
+    Node(const Node& other) : data(other.data), id(other.id) {}
+
+    // Default constructor
+    Node() {}
 };
 
+// Define the ArrayList class template with type T
 template <typename T>
 class ArrayList {
 private:
-	std::unique_ptr<Node<T>[]> mainList;
-	int size = 10, ip = 0;
-	const double RESIZE_THRESHOLD = 0.89;
+    // Unique pointer to an array of Node<T> objects
+    std::unique_ptr<Node<T>[]> mainList;
+
+    // The current size of the array list
+    int size;
+
+    // The index of the last element in the array list
+    int ip;
+
+    // The threshold for resizing the array list
+    const double RESIZE_THRESHOLD;
 
 public:
-	ArrayList(int capacity = 10) : mainList(std::make_unique<Node<T>[]>(capacity)), size(capacity) {}
+    // Default constructor with capacity set to 10
+    ArrayList(int capacity = 10)
+        : mainList(std::make_unique<Node<T>[]>(capacity)), size(capacity), ip(0), RESIZE_THRESHOLD(0.89) {}
 
-	ArrayList(const ArrayList& other) : ArrayList(other.size) {
-		std::copy(other.mainList.get(), other.mainList.get() + other.ip, mainList.get());
-		ip = other.ip;
-	}
+    // Copy constructor for creating a deep copy of another ArrayList
+    ArrayList(const ArrayList& other)
+        : ArrayList(other.size) {
+        std::copy(other.mainList.get(), other.mainList.get() + other.ip, mainList.get());
+        ip = other.ip;
+    }
 
-	ArrayList(ArrayList&& other) noexcept : mainList(std::move(other.mainList)), size(other.size), ip(other.ip) {
-		other.size = 0;
-		other.ip = 0;
-	}
+    // Move constructor for transferring ownership of another ArrayList's resources
+    ArrayList(ArrayList&& other) noexcept
+        : mainList(std::move(other.mainList)), size(other.size), ip(other.ip) {
+        other.size = 0;
+        other.ip = 0;
+    }
 
-	ArrayList& operator=(ArrayList other) {
-		swap(*this, other);
-		return *this;
-	}
+    // Copy assignment operator for swapping resources with another ArrayList
+    ArrayList& operator=(ArrayList other) {
+        swap(*this, other);
+        return *this;
+    }
 
-	void push(const T& data) {
-		if (((double)(ip + 1) / size) > RESIZE_THRESHOLD) {
-			resize();
-		}
-		mainList[ip].data = data;
-		mainList[ip].id = ip;
-		ip++;
-	}
+    // Push function for adding an element to the array list
+    void push(const T& data) {
+        if (((double)(ip + 1) / size) > RESIZE_THRESHOLD) {
+            resize();
+        }
+        mainList[ip].data = data;
+        mainList[ip].id = ip;
+        ip++;
+    }
 
-	void emplace(T&& data) {
-		if (((double)(ip + 1) / size) > RESIZE_THRESHOLD) {
-			resize();
-		}
-		mainList[ip].data = std::move(data);
-		mainList[ip].id = ip;
-		ip++;
-	}
+    // Emplace function for moving an element to the array list
+    void emplace(T&& data) {
+        if (((double)(ip + 1) / size) > RESIZE_THRESHOLD) {
+            resize();
+        }
+        mainList[ip].data = std::move(data);
+        mainList[ip].id = ip;
+        ip++;
+    }
 
-	void resize() {
-		size *= 2;
-		auto newList = std::make_unique<Node<T>[]>(size);
-		std::copy(mainList.get(), mainList.get() + ip, newList.get());
-		mainList = std::move(newList);
-	}
+    // Resize function for doubling the size of the array list
+    void resize() {
+        size *= 2;
+        auto newList = std::make_unique<Node<T>[]>(size);
+        std::copy(mainList.get(), mainList.get() + ip, newList.get());
+        mainList = std::move(newList);
+    }
 
-	void del(int index) {
-		if (index < 0 || index >= ip) {
-			std::cout << "Wrong index del" << std::endl;
-			return;
-		}
-		for (int i = index; i < ip - 1; i++) {
-			mainList[i] = mainList[i + 1];
-			mainList[i].id = i;
-		}
-		ip--;
-	}
+    // Delete function for removing an element at the given index
+    void del(int index) {
+        if (index < 0 || index >= ip) {
+            std::cout << "Wrong index del" << std::endl;
+            return;
+        }
+        for (int i = index; i < ip - 1; i++) {
+            mainList[i] = mainList[i + 1];
+            mainList[i].id = i;
+        }
+        ip--;
+    }
 
-	int getSize() const {
-		return ip;
-	}
+    // Get size function for retrieving the current size of the array list
+    int getSize() const {
+        return ip;
+    }
 
-	T get(int i) const {
-		if (i >= 0 && i < ip) {
-			return mainList[i].data;
-		}
-		throw std::out_of_range("Index out of range");
-	}
+    // Get function for retrieving the element at the given index
+    T get(int i) const {
+        if (i >= 0 && i < ip) {
+            return mainList[i].data;
+        }
+        throw std::out_of_range("Index out of range");
+    }
 
-	T& at(int i) {
-		if (i >= 0 && i < ip) {
-			return mainList[i].data;
-		}
-		throw std::out_of_range("Index out of range");
-	}
+    // At function for retrieving a reference to the element at the given index
+    T& at(int i) {
+        if (i >= 0 && i < ip) {
+            return mainList[i].data;
+        }
+        throw std::out_of_range("Index out of range");
+    }
 
-	const T& at(int i) const {
-		if (i >= 0 && i < ip) {
-			return mainList[i].data;
-		}
-		throw std::out_of_range("Index out of range");
-	}
+    // Const at function for retrieving a const reference to the element at the given index
+    const T& at(int i) const {
+        if (i >= 0 && i < ip) {
+            return mainList[i].data;
+        }
+        throw std::out_of_range("Index out of range");
+    }
 
-	void set(const T& data, int i) {
-		mainList[i].data = data;
-	}
+    // Set function for setting the value of the element at the given index
+    void set(const T& data, int i) {
+        mainList[i].data = data;
+    }
 
-	bool isEmpty() const {
-		return getSize() == 0;
-	}
+    // Is empty function for checking if the array list is empty
+    bool isEmpty() const {
+        return getSize() == 0;
+    }
 
-	bool contains(const T& data) const {
-		for (int i = 0; i < getSize(); i++) {
-			if (data == mainList[i].data) {
-				return true;
-			}
-		}
-		return false;
-	}
+    // Contains function for checking if the array list contains a specific element
+    bool contains(const T& data) const {
+        for (int i = 0; i < getSize(); i++) {
+            if (data == mainList[i].data) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	T& operator[](int i) {
-		return mainList[i].data;
-	}
+    // Overloaded index operator for retrieving a reference to the element at the given index
+    T& operator[](int i) {
+        return mainList[i].data;
+    }
 
-	const T& operator[](int i) const {
-		return mainList[i].data;
-	}
+    // Overloaded const index operator for retrieving a const reference to the element at the given index
+    const T& operator[](int i) const {
+        return mainList[i].data;
+    }
 
-	auto begin() {
-		return std::make_move_iterator(mainList.get());
-	}
+    // Begin function for retrieving a move iterator pointing to the first element
+    auto begin() {
+        return std::make_move_iterator(mainList.get());
+    }
 
-	auto end() {
-		return std::make_move_iterator(mainList.get() + ip);
-	}
+    // End function for retrieving a move iterator pointing to one past the last element
+    auto end() {
+        return std::make_move_iterator(mainList.get() + ip);
+    }
 
-	friend void swap(ArrayList& first, ArrayList& second) noexcept {
-		using std::swap;
-		swap(first.mainList, second.mainList);
-		swap(first.size, second.size);
-		swap(first.ip, second.ip);
-	}
+    // Friend swap function for swapping the resources of two ArrayList objects
+    friend void swap(ArrayList& first, ArrayList& second) noexcept {
+        using std::swap;
+        swap(first.mainList, second.mainList);
+        swap(first.size, second.size);
+        swap(first.ip, second.ip);
+    }
 };
